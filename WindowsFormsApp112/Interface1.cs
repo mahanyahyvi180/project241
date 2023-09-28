6739c5acbdf76b7bf8f15d969531415b92f40a7b
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-28 23:33:45 +0330

    Add files via upload

---
+++ WindowsFormsApp112/Interface1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp112
+{
+    public interface IEmployee
+    {
+
+        string FirstName
+        {
+            get;
+            set;
+        }
+        string lastName
+        {
+            get;
+            set;
+        }
+
+        string Concatinate();
+    }
+}
+
